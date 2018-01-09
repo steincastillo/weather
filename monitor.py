8b85ad08cede12c8f14faf84f8b706d3f9334858
steincastillo <stein@americamail.com>
2018-01-09 16:51:53 +0100

    Improved code structture

--- monitor.py
+++ monitor.py
@@ -40,6 +40,18 @@ def dbConnect(sqlite_file):
     c = conn.cursor()
     return conn, c
 
+def dbConnectRead(sqlite_file):
+    """ Make connection to an SQLite database READ ONLY mode """
+
+    # Validate  DB is available
+    if not(os.path.isfile(sqlite_file)):              
+        print ("[Error] File {} does not exist. Please verify\n".format(sqlite_file))
+        exit(0)
+    line = 'file:' + sqlite_file + '?mode=ro'
+    conn = sqlite3.connect(line, uri=True)
+    c = conn.cursor()
+    return conn, c
+
 def dbClose(conn):
     """ Commit changes and close connection to the database """
     
@@ -60,7 +72,6 @@ def animate(i):
     """ Animates the temperature chart """
     
     data = pd.read_sql_query(sql, conn)
-    #data = data[(len(data)-10):]
     xlist = []
     ylist = []
     for row in range(len(data)):
@@ -78,56 +89,58 @@ def animate(i):
     xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
     ax1.xaxis.set_major_formatter(xfmt)
     plt.xticks(rotation=30)
-    #fig.autofmt_xdate()
     ax1.plot(xlist, ylist, lw=2)
 
-# Construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser(description = "Display realtime weather readings")
-ap.add_argument("-a",
-                "--animate",
-                required = False,
-                default = False, 
-                type = str2bool,
-                help ="Animate the chart with realtime readings")
-args = vars(ap.parse_args())
-
-print(args)
-
-# Open DB read only mode
-conn = sqlite3.connect('file:weather.db?mode=ro', uri=True)
-c = conn.cursor()
-
-fig, ax1 = plt.subplots()
-
-sql = 'SELECT * FROM weather;'
-interval = 120000
-
-if args['animate']:
-    print ('[MSG] Dynamic chart displayed. Live update active')
-    print ('live update every {} seconds'.format(interval/1000))   
-    ani = animation.FuncAnimation(fig, animate, interval=interval)
-    plt.show()
-else:
-    data = pd.read_sql_query(sql, conn)
-    #data = data[(len(data)-10):]
-    xlist = []
-    ylist = []
-    for row in range(len(data)):
-        x = data['date'][row]
-        x = datetime.datetime.strptime(x, '%a %b %d %H:%M:%S %Y')
-        y = data['temperature'][row]
-        xlist.append(x)
-        ylist.append(y)
-    print ('[MSG] Static chart displayed. Live update disabled')
-    ax1.clear()
-    ax1.set_title('Temperature Monitor')
-    ax1.set_xlabel('Date/Time')
-    ax1.set_ylabel('Temp (C)')
-    ax1.grid(b=True, linestyle='dashed', color='grey')
-    xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
-    ax1.xaxis.set_major_formatter(xfmt)
-    plt.xticks(rotation=30)
-    #fig.autofmt_xdate()
-    ax1.plot(xlist, ylist, lw=2)
-    plt.show()
+# Main loop
+if __name__ == '__main__':
+    # Construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser(description = "Display realtime weather readings")
+    ap.add_argument("-a",
+                    "--animate",
+                    required = False,
+                    default = False, 
+                    type = str2bool,
+                    help ="Enable live update: [yes|no]")
+    args = vars(ap.parse_args())
+    
+    # Open DB read only mode
+    conn, cursor = dbConnectRead('weather.db')
+    
+    fig, ax1 = plt.subplots()
+    
+    sql = 'SELECT * FROM weather;'
+    interval = 120000
+    
+    if args['animate']:
+        print ('[MSG] Dynamic chart displayed. Live update active')
+        print ('live update every {} seconds'.format(interval/1000))
+        print ('*************************')   
+        ani = animation.FuncAnimation(fig, animate, interval=interval)
+        plt.show()
+    else:
+        data = pd.read_sql_query(sql, conn)
+        xlist = []
+        ylist = []
+        for row in range(len(data)):
+            x = data['date'][row]
+            x = datetime.datetime.strptime(x, '%a %b %d %H:%M:%S %Y')
+            y = data['temperature'][row]
+            xlist.append(x)
+            ylist.append(y)
+        print ('[MSG] Static chart displayed. Live update disabled')
+        print ('*************************') 
+        
+        # Draw statc chart
+        ax1.set_title('Temperature Monitor')
+        ax1.set_xlabel('Date/Time')
+        ax1.set_ylabel('Temp (C)')
+        ax1.grid(b=True, linestyle='dashed', color='grey')
+        xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
+        ax1.xaxis.set_major_formatter(xfmt)
+        plt.xticks(rotation=30)
+        ax1.plot(xlist, ylist, lw=2)
+        plt.show()
+        
+    # Close the database
+    dbClose(conn)
     
